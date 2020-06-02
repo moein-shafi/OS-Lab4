eefe28354ace7a4479bc9518e7a39ce7be5b2129
Ghazal <kalhorghazal1378@gmail.com>
2020-06-02 05:46:14 +0000

    Add resetsyscallnum and Refactoring

--- syscallcount.c
+++ syscallcount.c
@@ -5,9 +5,12 @@
 #define NUMBER_OF_PROCESSES 10
 #define MESSAGE_LENGTH 20
 #define NAME_LENGTH 4
+#define ZERO_STR '0'
+#define TEN 10
 #define ONE 1
 
-void reverse(char* str, int len) 
+void 
+reverse(char* str, int len) 
 { 
     int i = 0, j = len - 1, temp; 
     while (i < j) { 
@@ -20,32 +23,57 @@ void reverse(char* str, int len)
 } 
 
 
-int integer_to_string(int x, char str[], int d) 
+int 
+integer_to_string(int x, char str[], int d) 
 { 
     int i = 0; 
 
     if(x == 0)
-      str[i++] = '0';
+      str[i++] = ZERO_STR;
 
     while (x) { 
-        str[i++] = (x % 10) + '0'; 
-        x = x / 10; 
+        str[i++] = (x % TEN) + ZERO_STR; 
+        x = x / TEN; 
     } 
   
     while (i < d) 
-        str[i++] = '0'; 
+        str[i++] = ZERO_STR; 
   
     reverse(str, i); 
     str[i] = '\0'; 
     return i; 
 } 
 
+void
+call_file_syscalls(int index)
+{
+	char filename[NAME_LENGTH];
+	char message[MESSAGE_LENGTH] = "Per-cpu tests";
 
+	integer_to_string(index, filename, 0);
 
-int main(int argc, char const *argv[])
+	int fd = open(filename, O_CREATE | O_RDWR);
+
+	if(fd < 0) 
+	{
+        printf(1, "create file failed!\n");
+        exit();
+    }
+
+	if(write(fd, message, sizeof(message)) != sizeof(message))
+	{
+        printf(1, "write to file failed!\n");
+        exit();
+    }
+    close(fd);
+}
+
+int 
+main(int argc, char const *argv[])
 {
 	int pid = getpid();
 	int i;
+	resetsyscallnum();
 
 	for (i = ONE; i < NUMBER_OF_PROCESSES; ++i)
 	{
@@ -61,25 +89,7 @@ int main(int argc, char const *argv[])
     }
 	else if (pid == 0)
 	{
-		char filename[NAME_LENGTH];
-		char message[MESSAGE_LENGTH] = "Per-cpu tests";
-
-		integer_to_string(getpid(), filename, 0);
-
-		int fd = open(filename, O_CREATE | O_RDWR);
-
-		if(fd < 0) 
-		{
-        	printf(1, "create file failed!\n");
-        	exit();
-    	}
-
-		if(write(fd, message, sizeof(message)) != sizeof(message))
-		{
-        	printf(1, "write to file failed!\n");
-        	exit();
-    	}
-    	close(fd);
+		call_file_syscalls(getpid());
 	}
 	else
 	{
